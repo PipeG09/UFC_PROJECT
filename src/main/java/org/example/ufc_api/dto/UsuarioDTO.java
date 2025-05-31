bc0be922089020f60a29e14ca66ebe33a7bdd4b8
PipeG09 <103771722+PipeG09@users.noreply.github.com>
2025-05-31 15:18:51 -0300

    TODO FUNCIONANDO!

--- src/main/java/org/example/ufc_api/dto/UsuarioDTO.java
+++ src/main/java/org/example/ufc_api/dto/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package org.example.ufc_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class UsuarioDto {
@@ -9,11 +11,12 @@ public class UsuarioDto {
 
     private String correo;
 
-    private String rol;
-
     private LocalDateTime fechaCreacion;
 
     private String password;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String rol;
 
     public String getCorreo() {
         return correo;
@@ -47,19 +50,19 @@ public class UsuarioDto {
         this.nombre = nombre;
     }
 
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
     }
 }
